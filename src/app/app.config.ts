9d8396636222c27dc1034df2754a425212a0efa4
Giovanni La Scala <116294748+gyowyx96@users.noreply.github.com>
2026-01-12 16:54:40 +0100

    implement basic auth and polish directory

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, inject } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpHeaders, provideHttpClient } from '@angular/common/http';
 import { provideApollo } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
 import { InMemoryCache } from '@apollo/client';
@@ -14,9 +14,18 @@ export const appConfig: ApplicationConfig = {
     provideApollo(() => {
       const httpLink = inject(HttpLink);
 
+      // Inserisci qui le tue credenziali per la Basic Auth
+      const username = 'admin';
+      const password = 'supersecret';
+      const token = btoa(`${username}:${password}`); // Base64(username:password)
+
       return {
         link: httpLink.create({
-          uri: 'http://localhost:3306/graphql', // <- URL diretto al backend
+          uri: 'http://localhost:3306/graphql',
+          headers: new HttpHeaders({
+            Authorization: `Basic ${token}`,
+          }),
+          withCredentials: true, // necessario se il backend usa cors con credentials
         }),
         cache: new InMemoryCache(),
       };
